46c8b523aa69e6ce7841579a885439129a4613b1
PH <57695072+PHSix@users.noreply.github.com>
2024-08-02 21:49:32 +0800

    Update wezterm.lua

--- .config/wezterm_osx/wezterm.lua
+++ .config/wezterm_osx/wezterm.lua
@@ -1,23 +1,28 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+local c = wezterm.config_builder()
+
 return {
 	font = wezterm.font_with_fallback({
-		"Macon",
+		-- "Macon",
 		-- "Monaco",
-		"FiraMono Nerd Font",
+		-- "Maple Mono NF",
+		"JetBrains Mono",
 		"Symbols Nerd Font Mono",
+		"FiraCode Nerd Font",
 		"Noto Color Emoji",
 		"霞鹜文楷",
 		-- "ChillKai",
 		"monospace",
 	}),
-	font_size = 20,
+	use_fancy_tab_bar = false,
+	font_size = 22,
 	audible_bell = "Disabled",
 	enable_tab_bar = false,
 	-- color_scheme = "Nord (base16)",
-	-- color_scheme = "Edge Light (base16)",
-	color_scheme = "GitHub Dark",
+	color_scheme = "Edge Light (base16)",
+	-- color_scheme = "GitHub Dark",
 	window_padding = {
 		left = 0,
 		right = 0,
